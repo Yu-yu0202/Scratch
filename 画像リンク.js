6412376f965003794a04cceff50fef504009310d
Yu-yu0202 <135312165+Yu-yu0202@users.noreply.github.com>
2023-09-16 20:10:21 +0900

    Update 画像リンク.js

--- 画像リンク.js
+++ 画像リンク.js
@@ -1,16 +1,14 @@
 const url = location.href.split('/');
-if (url[url.length - 3] == "projects") {
+if(url[url.length - 3] == "projects") {
 	let project_id = url[url.length - 4];
 	let result = "https://scratch.mit.edu//../get%5image/project/" + project_id + "_5000x5000.png";
 	copyToClipboard(result);
 	alert("クリップボードへコピーしました。");
-}else{
-	if (url[url.length - 3] == "studios") {
+}else if(url[url.length - 3] == "studios") {
 		let studio_id = url[url.length - 4];
 		let result = "https://scratch.mit.edu//../get%5image/gallery/" + studio_id + "_5000x5000.png";
 		copyToClipboard(result);
 		alert("クリップボードへコピーしました。");
-	};
 }else{
 	alert("スタジオまたはプロジェクトページを開いてください。");
 }
