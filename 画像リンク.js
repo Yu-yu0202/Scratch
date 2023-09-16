d63cf079eca7120683936cfde6c745868c12adbe
Yu-yu0202 <135312165+Yu-yu0202@users.noreply.github.com>
2023-09-16 20:16:20 +0900

    Update 画像リンク.js

--- 画像リンク.js
+++ 画像リンク.js
@@ -1,14 +1,14 @@
-const url = location.href.split('/');
-if(url[url.length - 3] == "projects") {
-	let project_id = url[url.length - 4];
+const a = location.href.split('/');
+if(a[a.length - 3] == "projects") {
+	let project_id = a[a.length - 4];
 	let result = "https://scratch.mit.edu//../get%5image/project/" + project_id + "_5000x5000.png";
 	navigator.clipboard.writeText(result).then( ()=>{
 		alert("クリップボードにコピーしました！")
 	}, ()=>{
 		alert("コピーに失敗しました...")
 	});
-}else if(url[url.length - 3] == "studios") {
-		let studio_id = url[url.length - 4];
+}else if(a[a.length - 3] == "studios") {
+		let studio_id = a[a.length - 4];
 		let result = "https://scratch.mit.edu//../get%5image/gallery/" + studio_id + "_5000x5000.png";
 		navigator.clipboard.writeText(result).then( ()=>{
 			alert("クリップボードにコピーしました！")
