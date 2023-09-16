ccfb7b66648afba4c7c130f16ff30a9a60161fc3
Yu-yu0202 <135312165+Yu-yu0202@users.noreply.github.com>
2023-09-16 19:45:59 +0900

    Update 画像リンク.js

--- 画像リンク.js
+++ 画像リンク.js
@@ -4,13 +4,13 @@ if (url[url.length - 3] == "projects") {
 	let result = "https://scratch.mit.edu//../get%5image/project/" + project_id + "_5000x5000.png";
 	copyToClipboard(result);
 	alert("クリップボードへコピーしました。");
-} else {
+}else{
 	if (url[url.length - 3] == "studios") {
 		let studio_id = url[url.length - 4];
 		let result = "https://scratch.mit.edu//../get%5image/gallery/" + studio_id + "_5000x5000.png";
 		copyToClipboard(result);
 		alert("クリップボードへコピーしました。");
 	};
-} else {
+}else{
 	alert("スタジオまたはプロジェクトページを開いてください。");
 }
