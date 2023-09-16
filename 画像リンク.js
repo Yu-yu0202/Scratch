52df89e68d71575ad2550d1b6708c05e55db8155
Yu-yu0202 <135312165+Yu-yu0202@users.noreply.github.com>
2023-09-16 20:31:19 +0900

    ブックマークレッドでvarしか使わない理由がやっと分かった()

--- 画像リンク.js
+++ 画像リンク.js
@@ -1,15 +1,15 @@
-const a = location.href.split('/');
+var a = location.href.split('/');
 if(a[a.length - 3] == "projects") {
-	let project_id = a[a.length - 4];
-	let result = "https://scratch.mit.edu//../get%5image/project/" + project_id + "_5000x5000.png";
+	var project_id = a[a.length - 4];
+	var result = "https://scratch.mit.edu//../get%5image/project/" + project_id + "_5000x5000.png";
 	navigator.clipboard.writeText(result).then( ()=>{
 		alert("クリップボードにコピーしました！")
 	}, ()=>{
 		alert("コピーに失敗しました...")
 	});
 }else if(a[a.length - 3] == "studios") {
-		let studio_id = a[a.length - 4];
-		let result = "https://scratch.mit.edu//../get%5image/gallery/" + studio_id + "_5000x5000.png";
+		var studio_id = a[a.length - 4];
+		var result = "https://scratch.mit.edu//../get%5image/gallery/" + studio_id + "_5000x5000.png";
 		navigator.clipboard.writeText(result).then( ()=>{
 			alert("クリップボードにコピーしました！")
 		}, ()=>{
