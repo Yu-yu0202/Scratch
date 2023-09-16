ed6c76807917a0aef52ed4b70c68f5bfcd361769
Yu-yu0202 <135312165+Yu-yu0202@users.noreply.github.com>
2023-09-16 19:19:02 +0900

    Update 画像リンク.js

--- 画像リンク.js
+++ 画像リンク.js
@@ -1,7 +1,7 @@
 const url = location.href.sprit('/');
 if (url[url.length - 3] == "projects") {
 	let project_id = url[url.length - 4];
-	let result = "https://scratch.mit.edu//../get%5image/project/" + project_id + "_5000x5000.png"
+	let result = "https://scratch.mit.edu//../get%5image/project/" + project_id + "_5000x5000.png";
 	copyToClipboard(result);
 	alert("クリップボードへコピーしました。")
 } else {
