415522ec606e52f287ff40075925573a82d4db2a
Yu-yu0202 <135312165+Yu-yu0202@users.noreply.github.com>
2023-09-22 19:07:35 +0900

    ひゃっふう！

--- 画像リンク.js
+++ 画像リンク.js
@@ -1,20 +1,15 @@
 var a = location.href.split('/');
+var message = window.prompt("メッセージを入力してください。((略)/ここ/../get%5image/(略))", "");
 if(a[a.length - 3] == "projects") {
-	var project_id = a[a.length - 4];
-	var result = "https://scratch.mit.edu//../get%5image/project/" + project_id + "_5000x5000.png";
-	navigator.clipboard.writeText(result).then( ()=>{
-		alert("クリップボードにコピーしました！")
-	}, ()=>{
-		alert("コピーに失敗しました...")
-	});
+	var project_id = a[a.length - 2];
+	var result = "https://scratch.mit.edu/" + message + "/../get%5image/project/" + project_id + "_5000x5000.png";
+	navigator.clipboard.writeText(result);
+	alert("コピーしました！");
 }else if(a[a.length - 3] == "studios") {
-		var studio_id = a[a.length - 4];
-		var result = "https://scratch.mit.edu//../get%5image/gallery/" + studio_id + "_5000x5000.png";
-		navigator.clipboard.writeText(result).then( ()=>{
-			alert("クリップボードにコピーしました！")
-		}, ()=>{
-			alert("コピーに失敗しました...")
-		});
+		var studio_id = a[a.length - 2];
+		var result = "https://scratch.mit.edu/" + message + "/../get%5image/gallery/" + studio_id + "_5000x5000.png";
+		navigator.clipboard.writeText(result);
+		alert("コピーしました！");
 }else{
 	alert("スタジオまたはプロジェクトページを開いてください。");
 }
