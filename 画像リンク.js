1f6f279c924fec7cd64b6d505be40afa8506734c
Yu-yu0202 <135312165+Yu-yu0202@users.noreply.github.com>
2023-10-28 10:50:38 +0900

    「_」の文字コードが「%5」だったのを「%5f」に修正。

--- 画像リンク.js
+++ 画像リンク.js
@@ -1,13 +1,13 @@
 var a = location.href.split('/');
-var message = window.prompt("メッセージを入力してください。((略)/ここ/../get%5image/(略))", "");
+var message = window.prompt("メッセージを入力してください。((略)/ここ/../get%5fimage/(略))", "");
 if(a[a.length - 3] == "projects") {
 	var project_id = a[a.length - 2];
-	var result = "https://scratch.mit.edu/" + message + "/../get%5image/project/" + project_id + "_5000x5000.png";
+	var result = "https://scratch.mit.edu/" + message + "/../get%5fimage/project/" + project_id + "_5000x5000.png";
 	navigator.clipboard.writeText(result);
 	alert("コピーしました！");
 }else if(a[a.length - 3] == "studios") {
 		var studio_id = a[a.length - 2];
-		var result = "https://scratch.mit.edu/" + message + "/../get%5image/gallery/" + studio_id + "_5000x5000.png";
+		var result = "https://scratch.mit.edu/" + message + "/../get%5fimage/gallery/" + studio_id + "_5000x5000.png";
 		navigator.clipboard.writeText(result);
 		alert("コピーしました！");
 }else{
